dbae06da6b3c85de6816765753cb1f5cc658d965
Noah Spriggs <noahspriggs1@gmail.com>
2016-10-28 13:48:59 -0700

    base working

---
+++ speculo/app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var Phaser = require('./phaser.js');
+var app = express();
+
+var phaser = Phaser;
+
+var serv = require('http').Server(app);
+
+app.get('/',function(req, res) {
+  res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client',express.static(__dirname + '/client'));
+
+serv.listen(2000);
+console.log("Server started.");
+
+
+var io = require('socket.io')(serv,{});
+io.sockets.on('connection', function(socket){
+  console.log('socket connection');
+
+  socket.on('happy',function(data){
+    console.log('happy because ' + data.reason);
+  });
+
+  socket.emit('serverMsg',{
+    msg:'hello'
+  });
+
+});
